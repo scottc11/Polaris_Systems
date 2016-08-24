bad75b30fa35a33e80b4946ede6f8c3dbc3754f9
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-08-24 19:56:45 -0400

    product page still needs tab feature built.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 
+// when tab is clicked change tabs color
+// and show its content div
+// function displayContent(tabID) {
+//
+//
+//   // Get all elements with class="tabcontent" and hide them
+//   $('.tab-content').each( function() {
+//       $(this).removeClass('active');
+//     }
+//   );
+//
+//   // Show the current tab, and add an "active" class to the link that opened the tab
+//
+//   $('contentID').addClass('active');
+//
+// };
+//
+// $('#description-tab').on("click", displayContent( $(this).attr('id') ) );
 
 
-$(('#contact-name')).onClick.reset();
+
+
+
+// var $tab = $(this);
+//
+// if ($tab.hasClass('selected-tab')) { //check if it is currently selected
+//   return;
+// }
+// else {
+//   $('.selected-tab').removeClass('selected-tab');
+//   $tab.addClass('selected-tab');
+// }
