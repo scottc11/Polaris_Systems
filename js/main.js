d9aa7442e8222fc099340b2d9a24736e9ba0ebb4
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-08-25 15:31:47 -0400

    final commit before continuing development in a new repository for wordpress.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
-function showDescription() {
-  console.log("DESCRIPION");
 
+// Show Description tab
+function showDescription() {
+
+  // iterate through each tabs content, and hide them
   $('.tab-content').each( function() {
       $(this).removeClass('active');
     }
   );
 
+  // add an active class to the respective content tab
   $('#description-content').addClass('active');
 }
 
-function showSpecifications() {
-  console.log("SPECIFICATIONS");
 
+//show specifications tab
+function showSpecifications() {
+
+  // iterate through each tabs content, and hide them
   $('.tab-content').each( function() {
       $(this).removeClass('active');
     }
   );
 
+  // add an active class to the respective content tab
   $('#specifications-content').addClass('active');
 }
