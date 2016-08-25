1ad66d92328715546d57c88eb9c4ed31149f83e3
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-08-25 09:53:31 -0400

    finished building the tab boxes for the product content pages.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,21 @@
+function showDescription() {
+  console.log("DESCRIPION");
 
-// when tab is clicked change tabs color
-// and show its content div
-// function displayContent(tabID) {
-//
-//
-//   // Get all elements with class="tabcontent" and hide them
-//   $('.tab-content').each( function() {
-//       $(this).removeClass('active');
-//     }
-//   );
-//
-//   // Show the current tab, and add an "active" class to the link that opened the tab
-//
-//   $('contentID').addClass('active');
-//
-// };
-//
-// $('#description-tab').on("click", displayContent( $(this).attr('id') ) );
+  $('.tab-content').each( function() {
+      $(this).removeClass('active');
+    }
+  );
 
+  $('#description-content').addClass('active');
+}
 
+function showSpecifications() {
+  console.log("SPECIFICATIONS");
 
+  $('.tab-content').each( function() {
+      $(this).removeClass('active');
+    }
+  );
 
-
-// var $tab = $(this);
-//
-// if ($tab.hasClass('selected-tab')) { //check if it is currently selected
-//   return;
-// }
-// else {
-//   $('.selected-tab').removeClass('selected-tab');
-//   $tab.addClass('selected-tab');
-// }
+  $('#specifications-content').addClass('active');
+}
