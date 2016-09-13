1b6266d358baa5fb1d5498ef97a5dadfd76f9b94
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-09-13 11:01:38 -0400

    created sidebar button open and close effects for widget content.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
 
+
+// Products Sidebar buttons
+$('.expander').on('click', function() {
+
+  var expander = $(this);
+  var widgetContent = $(this).parent().parent().children('.widget-content');
+
+
+  // SHOWING THE WIDGET CONTENT
+  if (widgetContent.hasClass('hidden')) {
+
+    widgetContent.removeClass('hidden');
+    expander.find('.glyphicon').removeClass('glyphicon-triangle-bottom').addClass('glyphicon-triangle-top');
+    return;
+  }
+
+
+  // HIDING THE WIDGET CONTENT
+  if (widgetContent.hasClass('hidden') == false) {
+
+    widgetContent.addClass('hidden');
+    expander.find('.glyphicon').removeClass('glyphicon-triangle-top').addClass('glyphicon-triangle-bottom');
+    return;
+  }
+});
+
+// -----------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 // Show Description tab
 function showDescription() {
 
